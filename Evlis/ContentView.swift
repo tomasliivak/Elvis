0e6805a6434a7c40458130333a14318e215169c4
tomasliivak <tomasliivak@gmail.com>
2026-01-18 16:32:40 -0500

    Created the challenge

--- Evlis/ContentView.swift
+++ Evlis/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Spacer()
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.black)
+                .frame(width: 300, height: 300)
+            Text(message)
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Spacer()
+            HStack{
+                Button("Peace") {
+                    message = "Peace"
+                    imageName = "peacesign"
+                }
+                Button("Love") {
+                    message = "Love"
+                    imageName = "heart"
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    imageName = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
